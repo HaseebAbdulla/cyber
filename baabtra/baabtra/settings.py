856e49eccee908b30926758144fe8ed759ff7e8b
haseeb2222 <haseebabdulla@gmail.com>
2022-01-10 12:58:25 +0530

    'version4'

--- baabtra/baabtra/settings.py
+++ baabtra/baabtra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vqr@!kuhatxgpq$wrpa^f*o=&1v%61!muv!6f$38^u(ojhu(%+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
